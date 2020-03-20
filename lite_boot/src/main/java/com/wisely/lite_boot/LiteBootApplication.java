c94bee30b7f7d203d5b25d567473866b45096ea8
chrisnsw <649842096@qq.com>
2020-03-20 14:36:40 +0800

    initial lite_boot project

---
+++ lite_boot/src/main/java/com/wisely/lite_boot/LiteBootApplication.java
@@ -0,0 +1,13 @@
+package com.wisely.lite_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteBootApplication.class, args);
+    }
+
+}
---
+++ lite_boot/src/test/java/com/wisely/lite_boot/LiteBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisely.lite_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
